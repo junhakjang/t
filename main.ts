4e2176e877db87d0b249fba722404eedff40acb7
junhakjang <100299253+junhakjang@users.noreply.github.com>
2022-10-20 23:49:44 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
-function Backwads () {
-    pins.servoSetPulse(AnalogPin.P13, 1300)
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 1) {
+        Backwards()
+    }
+    if (receivedNumber == 2) {
+        Forward()
+    }
+    if (receivedNumber == 3) {
+        left()
+    }
+    if (receivedNumber == 4) {
+        Right()
+    }
+})
+function Forward () {
+    pins.servoSetPulse(AnalogPin.P8, 1300)
+    pins.servoSetPulse(AnalogPin.P13, 1700)
     control.waitMicros(20000)
 }
 function Sonar () {
@@ -13,35 +28,31 @@ function Sonar () {
 function left () {
     pins.servoSetPulse(AnalogPin.P8, 1700)
     control.waitMicros(2200000)
-    pins.servoSetPulse(AnalogPin.P8, 0)
-    Forward2()
-    Forward3()
+    pins.servoSetPulse(AnalogPin.P13, 0)
 }
 input.onButtonPressed(Button.A, function () {
-    Forward2()
-    Forward3()
+    Forward()
 })
-function Forward3 () {
-    pins.servoSetPulse(AnalogPin.P13, 1700)
-    control.waitMicros(20000)
-}
-function Forward2 () {
-    pins.servoSetPulse(AnalogPin.P8, 1700)
-    control.waitMicros(20000)
-}
 input.onButtonPressed(Button.B, function () {
-    Backwads()
-    Backwads2()
+    Backwards()
 })
+function Backwards () {
+    pins.servoSetPulse(AnalogPin.P8, 1700)
+    pins.servoSetPulse(AnalogPin.P13, 1300)
+    control.waitMicros(20000)
+}
+function Right () {
+    pins.servoSetPulse(AnalogPin.P13, 1700)
+    control.waitMicros(2200000)
+    pins.servoSetPulse(AnalogPin.P8, 0)
+}
 function Stop () {
     pins.servoSetPulse(AnalogPin.P13, 0)
     pins.servoSetPulse(AnalogPin.P8, 0)
 }
-function Backwads2 () {
-    pins.servoSetPulse(AnalogPin.P8, 1700)
-    control.waitMicros(20000)
-}
+let phantom_thing = 0
 let distanc = 0
+radio.setGroup(1)
 basic.showLeds(`
     . . . . .
     . . . . #
@@ -51,5 +62,16 @@ basic.showLeds(`
     `)
 distanc = 10
 basic.forever(function () {
-	
+    phantom_thing = 0
+    for (let index = 0; index < 4; index++) {
+        Sonar()
+        if (distanc <= 10) {
+            phantom_thing += 1
+        }
+    }
+    if (phantom_thing == 4) {
+        left()
+        Forward()
+        Forward()
+    }
 })
